409e0ad4376be99daf50c083c1e10c79b65c5ae9
Arya Daniswara Khairan <arya.daniswara@ui.ac.id>
2024-02-03 21:59:19 +0700

    add edit function

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -5,5 +5,7 @@ import java.util.List;
 
 public interface ProductService {
     public Product create(Product product);
+    public Product save(int index, Product product);
     public List<Product> findAll();
+    public int findIndex(String name);
 }
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ProductServiceImpl implements ProductService{
@@ -22,10 +23,29 @@ public class ProductServiceImpl implements ProductService{
     }
 
     @Override
+    public Product save(int index, Product product) {
+        productRepository.save(index, product);
+        return product;
+    }
+
+    @Override
     public List<Product> findAll() {
         Iterator<Product> productIterator = productRepository.findAll();
         List<Product> allProduct = new ArrayList<>();
         productIterator.forEachRemaining(allProduct::add);
         return allProduct;
+    }
+
+    @Override
+    public int findIndex(String name) {
+        int index = 0;
+        for (Iterator<Product> product = productRepository.findAll(); product.hasNext(); ) {
+            Product current = product.next();
+            if (Objects.equals(current.getProductName(), name)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
     }
 }
