4dd0d0990b57c9f281deb00a90f3c8c5e6cf277e
AryaDK153 <arya.daniswara@ui.ac.id>
2024-02-23 01:04:20 +0700

    fix logic

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -32,8 +32,7 @@ public class CarRepository {
         return null;
     }
     public Car update(String id, Car updatedCar) {
-        for (int i = 0; i < carData.size(); i++) {
-            Car car = carData.get(i);
+        for (Car car : carData) {
             if (car.getCarId().equals(id)) {
                 car.setCarName(updatedCar.getCarName());
                 car.setCarColor(updatedCar.getCarColor());
