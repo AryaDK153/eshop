c39382d0cfbc8d76c918268b6128f94d34bc5c53
AryaDK153 <arya.daniswara@ui.ac.id>
2024-03-01 06:01:46 +0700

    [RED] Add OrderRepository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -0,0 +1,22 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderRepository {
+    private List<Order> orderData = new ArrayList<>();
+
+    public Order save(Order order) {
+        return null;
+    }
+
+    public Order findById(String id) {
+        return null;
+    }
+
+    public List<Order> findAllByAuthor(String author) {
+        return null;
+    }
+}
