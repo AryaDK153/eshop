272d502e5fe3f374b4781f84939d46702fd1d16d
AryaDK153 <arya.daniswara@ui.ac.id>
2024-03-01 10:56:47 +0700

    fix autowiring on OrderRepository.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -1,10 +1,12 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Order;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class OrderRepository {
     private List<Order> orderData = new ArrayList<>();
 
