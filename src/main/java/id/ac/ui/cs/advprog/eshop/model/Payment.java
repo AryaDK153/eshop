75fc8b878c67a8324f8041eaba14dfe73fff7796
AryaDK153 <arya.daniswara@ui.ac.id>
2024-03-05 11:07:03 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,9 +1,10 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class Payment {
@@ -12,7 +13,54 @@ public class Payment {
     private Map<String,String> paymentData;
     private String status;
     
-    public Payment(String id, String method, Map<String, String> paymentData) {}
+    public Payment(String id, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
 
-    public void setStatus(String string) {}
+        String[] methodList = {"VOUCHER_CODE", "CASH_ON_DELIVERY"};
+        if (Arrays.stream(methodList).noneMatch(item -> (item.equals(method)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.method = method;
+        }
+
+        if (method.equals("VOUCHER_CODE")) {
+            String code = paymentData.get("voucherCode");
+            int numericCount = 0;
+            for (int i = 0; i < code.length(); i++) {
+                if (Character.isDigit(code.charAt(i))) {
+                    numericCount++;
+                }
+            }
+            if (code.length() != 16 || !code.startsWith("ESHOP") || numericCount != 8) {
+                this.status = "REJECTED";
+            } else {
+                this.status = "SUCCESS";
+            }
+        } else {
+            if (method.equals("CASH_ON_DELIVERY")) {
+                String address = paymentData.get("address");
+                if (address.isEmpty()) {
+                    this.status = "REJECTED";
+                } else {
+                    String deliveryFee = paymentData.get("deliveryFee");
+                    if (deliveryFee.isEmpty()) {
+                        this.status = "REJECTED";
+                    } else {
+                        this.status = "SUCCESS";
+                    }
+                }
+            }
+        }
+    }
+
+    public void setStatus(String status) {
+        String[] statusList = {"SUCCESS", "REJECTED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
+    }
 }
