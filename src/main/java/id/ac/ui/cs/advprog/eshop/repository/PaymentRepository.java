8696075e93022e57fb0b865e022f42481abda907
AryaDK153 <arya.daniswara@ui.ac.id>
2024-03-06 20:50:05 +0700

    [Green] redefine structure for Payment Repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,30 +1,33 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class PaymentRepository {
-    private List<Payment> paymentData = new ArrayList<>();
-    public Payment save(Payment payment) {
-        paymentData.add(payment);
-        return payment;
+    private List<Map<Payment, Order>> paymentData = new ArrayList<>();
+    public Map<Payment, Order> save(Map<Payment, Order> pair) {
+        paymentData.add(pair);
+        return pair;
     }
 
-    public Payment findById(String id) {
-        for (Payment payment : paymentData) {
+    public Map<Payment, Order> findById(String id) {
+        for (Map<Payment, Order> pair : paymentData) {
+            Payment payment = pair.keySet().stream().toList().get(0);
             if (payment.getId() == id) {
-                return payment;
+                return pair;
             }
         }
         return null;
     }
 
-    public Iterator<Payment> findAll() {
+    public Iterator<Map<Payment, Order>> findAll() {
         return paymentData.iterator();
     }
 }
